a464e3555aae7b18cae66f54304b4d92c42891f1
Leo Milligan <70490899+Milliganmaster@users.noreply.github.com>
2023-07-23 11:54:12 -0400

    Create spotifyTest.py

---
+++ spotifyTest.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from time import sleep
+
+DEVICE_ID="None"
+CLIENT_ID="b1d9811c8423481db3bf2545c54d9262"
+CLIENT_SECRET="d6233f3961b6444faad0c04ff236a9ad"
+
+# Spotify Authentication
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
+                                                client_secret=CLIENT_SECRET,
+                                                redirect_uri="http://localhost:8080",
+                                                scope="user-read-playback-state,user-modify-playback-state"))
+
+
+# Play the spotify track at URI with ID 45vW6Apg3QwawKzBi03rgD (you can swap this for a diff song ID below)
+sp.start_playback(device_id=None, uris=['spotify:track:5N0lmKQfQpXNQNjUGlPKIT'])
